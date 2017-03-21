12694a1d0669e6ac53ecbdb98d4ceb00ce847d27
andrewkmin <andrewmin8@gmail.com>
2017-03-20 20:02:26 -0700

    new models

--- models/models.js
+++ models/models.js
@@ -7,7 +7,7 @@ var connect = "mongodb://grove:grove@ds137100.mlab.com:37100/grovebackend";
 
 var userSchema = new mongoose.Schema({
   // userId provided by mongo and referred to later
-  username: {
+  email: {
     type: String,
     required: true
   },
@@ -28,67 +28,37 @@ var userSchema = new mongoose.Schema({
 
 // friends collection: collection of friend objects
 var friendSchema = new mongoose.Schema({
-  // friend ID is given by MongoDB
+  // _id is given by MongoDB
   userId: {
-    type: String,
-    required: true
+    type: String
   },
   fname: {
-    type: String,
-    required: true
-  },
+    type: String
+  }
   lname: {
-    type: String,
-    required: true
-  },
-  location: {
     type: String
-  },
+  }
   email: {
-    type: String,
-    required: true
-  },
-  role: {
     type: String
-  },
-  company: {
-    type: String
-  },
-  personality: {
-    type: String
-  },
-  sentiment: {
-    type: String
-  },
-  interests: {
-    type: String
-  },
-  met: {
-    type: String
-  },
-  action items: {
-    type: String
-  },
-  notes: {
-    type: String
+  }
+  tags: {
+    cat1: [],
+    cat2: [],
+    notes: {
+      type: String
+    }
   }
 });
 
 // BUCKETS
 var categorySchema = new mongoose.Schema({
-  // we need category ID: will be provided by Mongo.
+  // we need category ID: _id will be provided by Mongo.
   userId: { // should point back to original user
     type: String
   },
-  catType: {
-    type: String
-  },
-  catName: { // name of category
-    type: String
-    // need validation to have a name; otherwise "untitled"
-  },
-  friends: { // array of IDs
-    type: Array
+  tags: {
+    cat1: [],
+    cat2: []
   }
 });
 
